4f7c108c0c885a292d148fe91da0b7b55c76e174
teco1234 <tecosoueu@outlook.com>
2025-07-30 19:21:54 -0300

    jogo 1

--- j.js
+++ j.js
@@ -1,2 +1,11 @@
 
-function ele(a){return document.createElement(a)}
+
+function ig(a){return document.getElementById(a)}
+function ie(a){return document.createElement(a)}
+function ii(a,b){a.id=b}
+function ic(a,b){a.classList.add(b)}
+function it(a,b){a.textContent=b}
+function ap(a,b){a.appendChild(b)}
+
+function c0(a,b){a.style.backgroundColor=b}
+function c1(a,b){a.style.color=b}
